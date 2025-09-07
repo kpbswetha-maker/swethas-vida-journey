753b8b84a148896dc94ff4ad381230000d3bbec3
kpbswetha-maker <kpb.swetha@gmail.com>
2025-09-07 12:40:44 +0530

    added base directory and updated routing

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/swethas-vida-journey">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
