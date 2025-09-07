cb29a5f92beea4761c485b45392e0cf2ba7aa9b9
kpbswetha-maker <kpb.swetha@gmail.com>
2025-09-07 12:23:51 +0530

    Setting Base directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/swethas-vida-journey/",
   server: {
     host: "::",
     port: 8080,
